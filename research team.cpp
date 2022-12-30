8a88a901a4a69b856e5d5d67d22b68653c325379
lakshy1279 <46641127+lakshy1279@users.noreply.github.com>
2022-12-30 20:51:09 +0530

    Update research team.cpp
    Current solution fail for some cases.

--- research team.cpp
+++ research team.cpp
@@ -125,147 +125,125 @@ Output -
 4
 */
 
-#include <stdio.h>
-int Answer = 9999;
-int region[22][22];
-int visited[22][22];
-int N, C;
-int location[5][2];
-int rear = -1;
-int front = -1;
-struct queue {
-    int row;
-    int col;
-}Q[10000];
-
-void init()
-{
-    int m,n;
-    rear = -1;
-    front = -1;
-    for(m = 0; m < 22; m++)
-    {
-        for(n = 0; n < 22; n++)
-        {
-            visited[m][n] = 0;
-        }
-    }
-
-    for(m = 0; m < 10000; m++)
-    {
-        Q[m].row = 0;
-        Q[n].col = 0;
-    }
+#include<iostream>
+using namespace std;
+ 
+struct node{
+   int x;
+   int y;
+   int level;
+};
+ 
+node q[1000];
+int front = 0, back = 0;
+ 
+void init(){
+  front = back = 0;
 }
-
-void discover(int row, int col, int val)
-{
-    int l, m, k;
-    int cnt = 0;
-
-    for(k = 0; k < C; k++)
-    {
-        if(visited[location[k][0]][location[k][1]] > 0)
-            cnt++;
-    }
-    if(cnt >= C)
-        return;
-
-    if((row-1) >= 1 && visited[row-1][col] == 0 && (region[row-1][col] == 1 || region[row-1][col] == 3))
-    {
-        visited[row-1][col] = val+1;
-        ++rear;
-        Q[rear].row = row-1;
-        Q[rear].col = col;
-    }
-    if((row+1) <= N && visited[row+1][col] == 0 && (region[row+1][col] == 1 || region[row+1][col] == 3))
-    {
-        visited[row+1][col] = val+1;
-        ++rear;
-        Q[rear].row = row+1;
-        Q[rear].col = col;
-    }
-    if((col-1) >= 1 && visited[row][col-1] == 0 && (region[row][col-1] == 1 || region[row][col-1] == 3))
-    {
-        visited[row][col-1] = val+1;
-        ++rear;
-        Q[rear].row = row;
-        Q[rear].col = col-1;
-    }
-    if((col+1) <= N && visited[row][col+1] == 0 && (region[row][col+1] == 1 || region[row][col+1] == 3))
-    {
-        visited[row][col+1] = val+1;
-        ++rear;
-        Q[rear].row = row;
-        Q[rear].col = col+1;
-    }
-    
-    while(front<rear)
-    {
-        ++front;
-        discover(Q[front].row, Q[front].col, visited[Q[front].row][Q[front].col]);
-    }
-
+ 
+void push(int x, int y, int level){
+  q[back].x = x;
+  q[back].y = y;
+  q[back].level = level;
+  back++;
 }
-
-int main(void){
-    int T, test_case;
-
-    scanf("%d", &T);
-    for(test_case = 0; test_case < T; test_case++)
-    {
-        int i,j,k;
-        int x,y,c;
-        int temp = 0;
-
-        Answer = 9999;
-
-        scanf("%d%d", &N, &C);
-
-        for(i = 0; i < C; i++)
-        {
-            scanf("%d%d", &x, &y);
-            location[i][0] = x;
-            location[i][1] = y;
-        }
-
-        for(i = 1; i <= N; i++)
-        {
-            for(j = 1; j <= N; j++)
-            {
-                scanf("%d", &region[i][j]);
-            }
-        }
-        for(k = 0; k < C; k++)
-        {
-            region[location[k][0]][location[k][1]] = 3;
-        }
-
-        init();
-        Answer = 9999;
-        for(i = 1; i <= N; i++)
-        {
-            for(j = 1; j <= N; j++)
-            {
-                init();
-                temp = 0;
-                if(region[i][j] == 1)
-                {
-                    visited[i][j] = 1;
-                    discover(i, j, 1);
-                    for(k = 0; k < C; k++)
-                    {
-                        if(temp < visited[location[k][0]][location[k][1]])
-                            temp = visited[location[k][0]][location[k][1]];
-                    }
-                    if(Answer > temp)
-                        Answer = temp;
+node pop(){
+   return q[front++];
+}
+bool empty(){
+  return (front == back);
+}
+ 
+ 
+int a[100][100];
+int rare[4][2];
+int c;
+int n;
+ 
+bool valid(int r, int c){
+  return (r>=0 && r<n && c>=0 && c<n);
+}
+ 
+int vis[100][100];
+ 
+int xx[] = {-1,0,1,0};
+int yy[] = {0,1,0,-1};
+ 
+int bfs(int sx,int sy,int dx,int dy){
+ 
+    push(sx,sy,0);
+    vis[sx][sy] = 1;
+   
+    while(!empty()){
+ 
+        node temp = pop();
+        if(temp.x == dx && temp.y == dy)return temp.level;
+   
+        for(int i = 0; i<4; i++){
+ 
+            int valx = temp.x + xx[i];
+            int valy = temp.y + yy[i];
+            int lvl = temp.level + 1;
+ 
+            if(valid(valx,valy)){
+                if(a[valx][valy] == 1 && vis[valx][valy] == 0){
+                    push(valx,valy,lvl);
+                    vis[valx][valy] = 1;
                 }
-                
             }
         }
-        printf("#%d %d\n", test_case+1, Answer-1);
     }
-    return 0;
+   
 }
-
+ 
+ 
+int main(){
+ 
+   
+  int t; cin>>t;
+  while(t--){
+     cin>>n;
+     cin>>c;
+   
+     init();
+   
+    for(int i =0; i<c; i++){
+       int x,y;cin>>x>>y;
+       /*  indexing is so fucking important*/
+       x--;y--;
+       rare[i][0] = x;
+       rare[i][1] = y;
+    }
+   
+    for(int i = 0; i<n; i++){
+      for(int j =0; j<n; j++){
+        cin>>a[i][j];
+      }
+    }
+   
+    int ans = 10000;
+   
+    for(int i =0; i<n; i++){
+      for(int j =0; j<n; j++){
+         int temp;
+        if(a[i][j] == 1){
+            temp = 0;
+            
+            for(int k = 0; k<c; k++){
+                /*  fucking don't forgot to empty the queue */
+                init();
+              for(int l =0; l<100; l++)for(int m =0; m<100; m++)vis[l][m] = 0;
+               int res = bfs(i,j,rare[k][0],rare[k][1]);
+               temp = max(res,temp);
+            }
+            /* if k bahar mt likhio pehle galti se likh dia tha */
+             ans = min(ans,temp);
+        }
+       
+      }
+    }
+     cout<<ans<<endl;
+  }
+   
+  return 0;
+}
