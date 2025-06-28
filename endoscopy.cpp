e8df92bd06a33c863d5236b6e9440788c19f464b
Shantanu Singh <143492642+Shantanu-121@users.noreply.github.com>
2025-06-28 19:21:06 +0530

    Update endoscopy.cpp

--- endoscopy.cpp
+++ endoscopy.cpp
@@ -173,3 +173,113 @@ int main(){
 	}
 	return 0;
 }
+
+// Simple Bfs, just check the type of pipe then select it's neighbors.. and check if neighbors allow reverse movement (pipe compatibility.. eg. Up-Down pipe has adjacent neighbor as left-right pipe then 
+// it pipes ain't compatible.. endoscopy can't be performed)
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define ll long long int
+// #define vi vector<int>
+// #define pii pair<int, int>
+// #define F first
+// #define S second
+// #define input(a)                       \
+//     for (int i = 0; i < a.size(); i++) \
+//     cin >> a[i]
+// #define print(a)     \
+//     for (auto i : a) \
+//     cout << i << ' '
+// #define all(a) a.begin(), a.end()
+
+// bool isValid(int r, int c, vector<vector<int>> &a)
+// {
+//     return (r >= 0 && c >= 0 && r < a.size() && c < a[0].size());
+// }
+
+// void solve()
+// {
+//     int h, w;
+//     cin >> h >> w;
+//     pii st;
+//     cin >> st.first >> st.second;
+//     int l;
+//     cin >> l;
+
+//     vector<vector<int>> gmap(h, vector<int>(w));
+//     for (int i = 0; i < h; i++)
+//         for (int j = 0; j < w; j++)
+//             cin >> gmap[i][j];
+
+//     if (gmap[st.first][st.second] == 0)
+//     {
+//         cout << 0 << endl;
+//         return;
+//     }
+
+//     vector<vector<bool>> vis(h, vector<bool>(w, 0));
+//     int res = 1;
+//     vis[st.first][st.second] = 1;
+//     l--;
+
+//     queue<pair<int, int>> q;
+//     q.push({st.first, st.second});
+
+//     int dr[] = {-1, 1, 0, 0};
+//     int dc[] = {0, 0, -1, 1};
+//     int opposite[] = {1, 0, 3, 2};
+
+//     vector<vector<int>> pipe = {
+//         {},
+//         {0, 1, 2, 3},
+//         {0, 1},
+//         {2, 3},
+//         {0, 3},
+//         {1, 3},
+//         {1, 2},
+//         {0, 2}};
+
+//     while (l > 0 && !q.empty())
+//     {
+//         int size = q.size();
+//         while (size--)
+//         {
+//             auto top = q.front();
+//             int r = top.first;
+//             int c = top.second;
+//             q.pop();
+//             int pipetype = gmap[r][c];
+
+//             for (int dir : pipe[pipetype])
+//             {
+//                 int nr = r + dr[dir];
+//                 int nc = c + dc[dir];
+
+//                 if (isValid(nr, nc, gmap) && !vis[nr][nc] && gmap[nr][nc] != 0)
+//                 {
+//                     int neighborType = gmap[nr][nc];
+//                     if (find(pipe[neighborType].begin(), pipe[neighborType].end(), opposite[dir]) != pipe[neighborType].end())
+//                     {
+//                         vis[nr][nc] = 1;
+//                         res++;
+//                         q.push({nr, nc});
+//                     }
+//                 }
+//             }
+//         }
+//         l--;
+//     }
+
+//     cout << res << endl;
+// }
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     int t = 1;
+//     cin >> t;
+//     while (t--)
+//         solve();
+//     return 0;
+// }
